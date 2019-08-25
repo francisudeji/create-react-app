5de521a2fbd07ddf634295a85aaf589982e8dc62
francisudeji <francis.udejiofficial@gmail.com>
2019-08-25 10:12:31 +0100

    Display links in home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Container, H1, H2, ButtonGroup } from '../styles'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <Container>
+      <H1>Front-end Code Challenge for Scoutbase</H1>
+
+      <H2>Routes</H2>
+      <ButtonGroup>
+        <Link to='/countries'>/countries</Link>
+        <Link to='/countries/AD'>/countries/AD</Link>
+      </ButtonGroup>
+    </Container>
   )
 }
 
